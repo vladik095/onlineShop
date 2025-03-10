58096fcd2bfaa9a00803ff707cced6ceec11c939
Vladislav <ilusevicv@gmail.com>
2025-03-11 01:14:21 +0300

    upgrade congig

--- app/config.py
+++ app/config.py
@@ -2,23 +2,20 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    DB_HOST: str
     DB_PORT: str
     DB_USER: str
+    DB_HOST: str
     DB_PASS: str
     DB_NAME: str
 
     @property
     def DATABASE_URL(self) -> str:
         return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
-    
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 1
+    SECRET_KEY: str
+    ALGORITHM: str
     class Config:
         env_file = ".env"
 
 settings = Settings()
-
-print("DB_HOST:", settings.DB_HOST)
-print("DB_PORT:", settings.DB_PORT)
-print("DB_USER:", settings.DB_USER)
-print("DB_PASS:", settings.DB_PASS)
-print("DB_NAME:", settings.DB_NAME)
+print(settings)
